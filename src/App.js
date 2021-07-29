2afa8e59f1e6e33bb5f6a35cac04483e4fadb11a
Sabbir Ahmed <39632780+Sabbirmadman@users.noreply.github.com>
2021-07-30 03:11:37 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -106,7 +106,7 @@ function ChatRoom() {
 
       <input value={formValue} onChange={(e) => setFormValue(e.target.value)} placeholder="type here" />
 
-      <button type="submit" disabled={!formValue}>🕊️</button>
+      <button type="submit" disabled={!formValue}>'send >'</button>
 
     </form>
   </>)
